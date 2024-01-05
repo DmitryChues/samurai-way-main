73a630e0f0e3ba5851b9343e2114dee99c9b1b96
Dmitry <dmitrychues2312@gmail.com>
2024-01-05 16:00:22 +0300

    1-13

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<header className='header'>
+			<img src="https://lh3.googleusercontent.com/3bXLbllNTRoiTCBdkybd1YzqVWWDRrRwJNkVRZ3mcf7rlydWfR13qJlCSxJRO8kPe304nw1jQ_B0niDo56gPgoGx6x_ZOjtVOK6UGIr3kshpmTq46pvFObfJ2K0wzoqk36MWWSnh0y9PzgE7PVSRz6Y" alt="logo" />
+		</header>
+	);
+};
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src="https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?cs=srgb&dl=pexels-kellie-churchman-1001682.jpg&fm=jpg" alt="sea" />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					<div>post 1</div>
+					<div>post 2</div>
+				</div>
+			</div>
+		</div>
+	);
+};
